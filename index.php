75e8f67294523e253e366cef3e36cbda70f89eca
Thao301 <225481015@vaa.edu.vn>
2023-09-29 03:40:57 +0700

    sua logo

--- index.php
+++ index.php
@@ -1,53 +1,324 @@
-
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <link rel="stylesheet" href="./css/style.css"> 
-    <link rel="stylesheet" href="./css/checkphong.css"> 
-    <link rel="stylesheet" href="./css/menu-logo.css"> 
-    <link rel="stylesheet" href="./css/loaiphong.css">  
-    <link rel="stylesheet" href="./css/phongnoibat_dv.css">
-    <link rel="stylesheet" href="./css/diadiemnb.css">   
-    <link rel="stylesheet" href="./css/danhgia.css"> 
-    <link rel="stylesheet" href="./css/mota-uudai.css"> 
-    <?php 
-$link=new mysqli("localhost", "root","","khachsan");
-$sql="select * from loaiphong";
-$result=$link->query($sql);
-?>
-     
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <link rel="stylesheet" href="./style/main.css"/>
+    <title>Page Title</title>
+  <style>
+    .pnb_anh {
+  width: 23%;
+  float: left;
+  height: 70%;
+  margin: 20px 8px 0 8px;
+  border: 1px dashed #ff69b4;
+}
+.dv_anh1 {
+  width: 40%;
+  float: left;
+  height: 30%;
+  margin: 20px 15px 10px 50px;
+  border: 1px dashed #ff69b4;
+}
+.dv_anh2 {
+  width: 40%;
+  float: right;
+  height: 30%;
+  margin: 20px 69px 10px 8px;
+  border: 1px dashed #ff69b4;
+}
+.phong_noi_bat {
+  position: relative;
+  height: 240px;
+  width: 98%;
+  padding: 30px 15px 30px 15px;
+}
+.dich_vu {
+  position: relative;
+  height: 1140px;
+  width: 98%;
+  padding: 20px 30px 80px 30px;
+}
+.lable {
+  text-align: center;
+}
+.container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.overlay {
+  position: absolute;
+  width: 80%;
+  height: 10%;
+  top: 610px;
+  left: 145px;
+  background-color: whitesmoke;
+}
+.check {
+  width: 22%;
+  margin-top: 1%;
+  display: inline-block;
+}
+.check1 {
+  height: 20px;
+  margin-right: 17%;
+  margin-left: -13%;
+  background-color: rgb(252, 195, 205);
+}
+.check2 {
+  height: 31px;
+  margin-right: 17%;
+  margin-left: -13%;
+  background-color: rgb(215, 234, 240);
+}
+.timkiem {
+position: absolute;
+top: 12px;
+right: 13px;
+font-size: 14px;
+background-color:  lightskyblue;
+width: 90px;
+height: 51px;
+border: none;
+border-radius: 5px;
+transition: all 0.5s;
+-webkit-transition: all 0.5s;
+
+}
+
+.timkiem:hover {
+background-color: #6fc5ff;
+box-shadow: 0 0 20px #6fc5ff50;
+transform: scale(1.1);
+}
+
+.timkiem:active {
+background-color: #3d94cf;
+transition: all 0.25s;
+-webkit-transition: all 0.25s;
+box-shadow: none;
+transform: scale(0.98);
+}
+.logo1 {
+  float: left;
+  width: 10%;
+  height: 50px;
+  cursor: pointer;
+  line-height: 3;
+  text-align: center;
+}
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+.layout_danh_gia_noi_bat{
+  float: left;
+  width: 100%;
+  height: 400px;
+  background-color: rgb(233, 205, 186);
+}
+.danh_gia{
+  float: left;
+  width: 40%;
+  height: 200px;
+  margin: 3% 0% 3% 7%;
+  border-radius: 20px;
+  background-color: rgb(250, 244, 193);
+}
+.anh_dai_dien{
+  float: left;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  margin: 2%;
+}
+.comment{
+  float: left;
+  width: 70%;
+  height: 150px;
+  margin: 5% 3% 3% 3%;
+}
+.loi_goi{
+  float: left;
+  width: 100%;
+  height: 170px;
+  text-align: center;
+  margin-top: 3%;
+}
+.dat_phong{
+  width: 75%;
+  height: 80px;
+  position: relative;
+  left: 13%;
+  top: 575px;
+  background-color: rgb(156, 130, 130);
+}
+.dat_phong1{
+  float: left;
+  position: absolute;
+  width: 24%;
+  height: 60px;
+  background-color: #fff4d4;
+}
+.dat_phong2{
+  float: left;
+  position: absolute;
+  width: 21%;
+  height: 60px;
+  background-color: rgb(120, 106, 71);
+}
+.layout_thong_tin{
+  float: left;
+  width: 100%;
+  height: 450px;
+  background-color: rgb(243, 222, 169);
+}
+.thong_tin{
+  float: left;
+  width: 20%;
+  height: 250px;
+}
+.menu{
+  width: 200px;
+  background-color: rgb(154, 197, 235);
+  position: absolute;
+  top: 50px;
+  z-index: 1;
+  display: none;
+}
+.menu a{
+  padding: auto;
+  display: block;
+  text-decoration: none;
+}
+
+.diadiemnoibat {
+  width: 100%;
+  height: 420px;
+  background: rgb(186, 188, 184);
+  border: 2px solid black;  
+}
+.circle {
+  width: 300px;   
+  height: 300px;
+  background: white;
+  border-radius: 50%;  
+}
+.anhdiadiem {
+  width: 100%;
+  height: 320px;
+  background: rgb(186, 188, 184);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: start;
+}      
+  
+.logo1:hover .menu{display: block;}
+.menu a:hover {background-color: lightblue;}
+.logo1:hover {background-color: darkgray}
+.mota1{
+width: 100% ; 
+height: 800px ; 
+border: 2px solid black
+}
+
+.mota2{
+width: 100% ; 
+height: 620px ; 
+border-bottom: 2px solid black
+}
+
+.mota3{
+float: left ;
+width: 50% ;
+height: 620px
+}
+
+.mota4{
+margin: 50px 10% 50px 10% ; 
+border: 2px solid black
+}
+
+.mota5{
+width: 100% ; 
+height: 220px ; 
+border-bottom: 2px solid black
+}
+
+.mota6{
+width: 100% ; 
+height: 299px
+}
+
+.mota7{
+float: right ; 
+width: 50% ; 
+height: 620px
+}
+
+.uudai1{
+width: 100% ; 
+height: 180px
+}
+
+.uudai2{
+width: 100% ; 
+height: 20px
+}
+
+.uudai3{
+float: left ; 
+width: 50% ; 
+height: 160px
+}
+
+.uudai4{
+float: right ; 
+width: 50% ; 
+height: 160px
+}
+
+.uudai5{
+margin: 20px 20% 20px 20% ; 
+height: 100px ; 
+border: 2px solid black
+}
+  </style>
   </head>
   <body>
-   <div class="container"> 
-     <div style="float: center; width: 100%; position: fixed; z-index: 1;">
-      <div align="center" class="logo1" style="background-color: rgb(109, 194, 230);" ;="">
-        <p><font face="Courier New" size="5" color="WHITE">ROOMS</font></p>
-        <div class="menu">
-          
-        <?php
-while ($row=$result->fetch_assoc())
-{
- ?>   
-    <a> <?php echo $row["LOAIPHONG"];?></a>
-<?php
- }
-?>
-           </div>
-      </div>
-        <div align="center" class="logo1" style="background-color: rgb(28, 107, 225)" ;=""></div>
-        <div align="center" class="logo1" style="background-color: rgb(106, 160, 193)";=""></div>
-        <div align="center" style="float: left; width: 40%; height: 50px; background-color: rgb(237, 240, 241); ";=""> LOGO</div>
-        <div align="center" class="logo1" style="background-color: rgb(14, 157, 197)";=""></div>
-        <div align="center" class="logo1" style="background-color: rgb(23, 53, 151)" ;=""></div>
-        <div align="center" class="logo1" style="background-color: rgb(53, 91, 101)";=""></div>
-      </div>
-        <div
-          align="center" style="width: 100%; height: 600px; background-color: rgb(194, 233, 255); position:relative; top:50px">ẢNH
+    <div class="container">
+      <div style="width: 100%; position: fixed; z-index: 1; background-color:gray">
+        <div class="logo1" style="margin-left: 12%" ;="">Phòng
+          <div class="menu">
+            <a href="http://localhost/khachsan/">standard room</a>
+            <a href="http://localhost/khachsan/">superior room</a>
+            <a href="http://localhost/khachsan/">deluxe room</a>
+            <a href="http://localhost/khachsan/">family suite</a>
+          </div>
         </div>
+        <div class="logo1"></div>
+        <div class="logo1";=""></div>
+        <div style="
+            float: left;
+            width: 15%;
+            height: 50px;
+            background-color: rgb(237, 240, 241);
+            text-align:center;
+            line-height:3
+          " ;="">
+          LOGO
+        </div>
+        <div class="logo1";=""></div>
+        <div class="logo1";=""></div>
+        <div class="logo1";=""></div>
+      
+      </div>
+      <div align="center" style="width: 100%; height: 600px; background-color: rgb(194, 233, 255); position:relative; top:50px">ẢNH
+      </div>
 
-
-        <div style="width: 100%; height: 620px; margin-top: 5%;">
+      <div style="width: 100%; height: 620px; margin-top: 5%;">
         <div class="mota2"> 
           <div class="mota3"> 
               <div align="center" class="mota4">
@@ -63,7 +334,6 @@ while ($row=$result->fetch_assoc())
           </div>
       </div>
       </div>
-      
       <div align="center" class="overlay">
         <div class="check">
           <div class="check1">ĐIỂM ĐẾN</div>
@@ -81,10 +351,10 @@ while ($row=$result->fetch_assoc())
           <div class="check1"> số phòng và khách</div>
           <div class="check2">1phòng, 2khach</div>
         </div>
-        <button class="timkiem" onclick="alert('kakak')">CHECK ROOM</button> 
-      </div>  
-
-            <div class="uudai1">
+        &gt;
+        <button class="timkiem" onclick="alert(kakak39)">CHECK ROOM</button>
+      </div>
+      <div class="uudai1">
         <div align="center" class="uudai2"> Ưu Đãi </div>
         <div class="uudai3">
             <div align="center" class="uudai3">
@@ -107,7 +377,7 @@ while ($row=$result->fetch_assoc())
             </div>
         </div>
     </div>
-    
+
       <br>
       <div class="phong_noi_bat">
         <h3 class="lable">PHONG NOI BAT</h3>
@@ -129,94 +399,125 @@ while ($row=$result->fetch_assoc())
         <div class="dv_anh2">. . . Ả N H</div>
       </div>
 
-
-
-    <div class="loaiphong" >
+      <div align="left" style="
+          width: 100%;
+          border: 1px solid black;
+          height: 300px;
+          background-color: rgb(21, 191, 21);
+        ">
         <b>LOẠI PHÒNG</b>
-      <div class="loaiphong3" style=" margin-top: 40px; margin-right: 30px">
-        <div class="loaiphong2">Ảnh</div>
-      </div>
-      <div class="loaiphong1">
-        <div class="loaiphong2">Ảnh</div>
-      </div>
-      <div class="loaiphong1">
-        <div class="loaiphong2">Ảnh</div>
-      </div>
-      <div class="loaiphong3" style=" margin-top: 22px; margin-right: 10px">
-        <div class="loaiphong2">Ảnh</div>
-      </div>
-    </div>
+        <div style="width: 23%; float: right; margin-top: 40px; margin-right: 30px">
+          <div align="center" style="
+              border: 1px solid black;
+              height: 200px;
+              background-color: white;
+            ">
+            Ảnh
+          </div>
+        </div>
+        <div style="width: 23%; float: right; margin-top: 40px; margin-right: 10px">
+          <div align="center" style="
+              border: 1px solid black;
+              height: 200px;
+              background-color: white;
+            ">
+            Ảnh
+          </div>
+        </div>
+        <div style="width: 23%; float: right; margin-top: 40px; margin-right: 10px">
+          <div align="center" style="
+              border: 1px solid black;
+              height: 200px;
+              background-color: white;
+            ">
+            Ảnh
+          </div>
+        </div>
+        <div style="width: 23%; float: right; margin-top: 22px; margin-right: 10px">
+          <div align="center" style="
+              border: 1px solid black;
+              height: 200px;
+              background-color: white;
+            ">
+            Ảnh
+          </div>
+        </div>
+        </div>
 
-    
-  <div class="diadiemnoibat">
-        <h4  >ĐỊA ĐIỂM NỔI BẬT</h4>
-    <div class="anhdiadiem">
-      <div class="circle"></div>
-      <div class="circle"></div>
-      <div class="circle"></div>
-      <div class="circle"></div>
-    </div>
-  </div>
-    
+      
+        <div class="diadiemnoibat">
+          <h2 style="text-align: center" >ĐỊA ĐIỂM NỔI BẬT</h2>
+            <div class="anhdiadiem">
+              <img class="circle" src="../Imgs/ba.jpg" alt="Bữa ăn view biển" width="320" height="320">
+              <img class="circle" src="../Imgs/nd.jpeg" alt="Trò chơi cảm giác mạnh " width="320" height="320">
+              <img class="circle" src="../Imgs/ptv.jpg" alt="Phòng vip" width="320" height="320">
+              <img class="circle" src="../Imgs/bad.jpg" alt="Nhà hàng" width="320" height="320">
+            </div>
+      </div>
 
       <div class="layout_danh_gia_noi_bat">
         <h1 style="text-align: center"><b>ĐÁNH GIÁ NỔI BẬT</b></h1>
         <div class="danh_gia">
-          <div class="anh_dai_dien"></div>
+          <img class="anh_dai_dien" src="https://i.pinimg.com/736x/63/a2/e2/63a2e222c202f9035cb38a7151ad8f19.jpg">
           <div class="comment">
             klrh uiahvna ;onakwe jhhF HVMNLK AWkesjrghrj vknak vknak jnk;jna;
             knjn lkfqnlwk lawnli lksjf vknak jnk;jna; knjn lkfqnlwk lawnli lksjf
             jnk;jna; knjn lkfqnlwk lawnli lksjfalw liawjj roanwnr onoiEkjdc nc wjj
-            ammwlkcnln cnken jkachuiwh lcneuh akehc H[ OISH;I uhiy yguy guyg it7i 
+            ammwlkcnln cnken jkachuiwh lcneuh akehc H[ OISH;I uhiy yguy guyg it7
+            <div style="margin:2%">⭐⭐⭐⭐⭐</div>
+            <div><b>- Lalisa -</b></div>
           </div>
+          
         </div>
 
         <div class="danh_gia">
-          <div class="anh_dai_dien"></div>
+        <img class="anh_dai_dien" src="https://i.pinimg.com/originals/cd/b2/3f/cdb23fa36c904fb5efdcff3962fb86a5.jpg">
           <div class="comment">
             klrh uiahvna ;onakwe jhhF HVMNLK AWkesjrghrj vknak vknak jnk;jna;
             knjn lkfqnlwk lawnli lksjf vknak jnk;jna; knjn lkfqnlwk lawnli lksjf
             jnk;jna; knjn lkfqnlwk lawnli lksjfalw liawjj roanwnr onoiEH[ OISH;I
+            <div style="margin:2%">⭐⭐⭐⭐⭐</div>
+            <div><b>- Jennie -</b></div>
           </div>
         </div>
 
       </div>
       <div class="loi_goi">
         <h3>HÃY BẮT ĐẦU TRẢI NGHIỆM </h3>
-        <h2>TÊN</h2>
+        <h2>LOTUS</h2>
       </div>
       <div class="dat_phong">
         <div class="dat_phong1" style="top: 10px;left: 2%;"></div>
-        <div class="dat_phong1" style="top: 10px;left: 38%;"></div>
-        <div class="dat_phong2" style="top: 10px;left: 75%;"></div>
+        <div class="dat_phong1" style="top: 10px;left: 27%;"></div>
+        <div class="dat_phong1" style="top: 10px;left: 52%;"></div>
+        <div class="dat_phong2" style="top: 10px;left: 77%;"></div>
       </div>
       <div class="layout_thong_tin">
-        <div class="thong_tin">
-          <h1>Ten KS</h1>
+        <div class="thong_tin" style="margin: 5% 2% 0 8%">
+          <h1>LOTUS</h1>
           klrh uiahvna ;onakwe jhhF HVMNLK AWkesjrghrj vknak vknak jnk;jna; knjn
           lkfqnlwk lawnli lksjf vknak jnk;jna; knjn lkfqnlwk lawnli lksjf
           jnk;jna; knjn lkfqnlwk lawnli lksjfalw liawjj roanwnr onoiEH[ OISH;I
         </div>
-        <div class="thong_tin">
+        <div class="thong_tin" style="margin: 5% 2% 0 0">
           <h1>Lien He</h1>
-          0316546156 <br />
+          0316546156 <br>
           klrh uiahvna ;onakwe jhhF HVMNLK AWkesjrghrj vknak vknak jnk;jna; knjn
           lkfqnlwk lawnli lksjf vknak jnk;jna; knjn lkfqnlwk lawnli lksjf
           jnk;jna; knjn lkfqnlwk lawnli lksjfalw liawjj roanwnr onoiEH[ OISH;I
         </div>
-        <div class="thong_tin">
+        <div class="thong_tin" style="margin: 5% 2% 0 0">
           <h1>Quick Links</h1>
           klrh uiahvna ;onakwe jhhF HVMNLK AWkesjrghrj vknak vknak jnk;jna; knjn
           lkfqnlwk lawnli lksjf vknak jnk;jna; knjn lkfqnlwk lawnli lksjf
           jnk;jna; knjn lkfqnlwk lawnli lksjfalw liawjj roanwnr onoiEH[ OISH;I
         </div>
-        <div class="thong_tin">
+        <div class="thong_tin" style="margin: 5% 0 0 0">
           <h1>Apps</h1>
-          klrh uiahvna ;onakwe jhhF HVMNLK AWkesjrghrj vknak vknak jnk;jna; knjn
-          lkfqnlwk lawnli lksjf vknak jnk;jna; knjn lkfqnlwk lawnli lksjf
-          jnk;jna; knjn lkfqnlwk lawnli lksjfalw liawjj roanwnr onoiEH[ OISH;I
+          <img src="https://laseta.bithemer.com/assets/img/icons/i1.svg">
+          <img src="https://laseta.bithemer.com/assets/img/icons/i2.svg">
         </div>
       </div>
     </div>
-  </body>
-</html>
+
+</body></html>
