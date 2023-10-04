fb7a6bb0270514daba0db505c6f2a940f76470ba
Thecoffeemint <haidang@vtv.vn>
2023-10-04 09:41:57 +0700

    update phongnoibat picture

--- Homepage/html/phongnoibat.php
+++ Homepage/html/phongnoibat.php
@@ -4,7 +4,7 @@
         <ul class= "ul">
               <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="0" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/11.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/1.jpg" >
         </a>
       </li>
           <li >
@@ -29,37 +29,37 @@
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="5" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/6.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/6.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="6" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/7.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/7.jpg" >
         </a>
       </li>
           <li >
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="7" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/12.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/8.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="8" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/10.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/9.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="9" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/3.jpg" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/10.jpg" >
         </a>
       </li>
           <li >
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="10" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/1.jpg" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/11.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="11" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/8.webp" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/12.jpg" >
         </a>
       </li>
           <li class=" dau">
@@ -89,82 +89,82 @@
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="17" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/1.jpg" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/18.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="18" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/2.jpg" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/19.jpg" >
         </a>
       </li>
           <li class="image fancy nh-khch-sn portrait">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="19" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/10.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/20.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="20" >
-          <img class="hinhanh" alt=" hinhanh" src="img/anhphongnoibat/4.jpg" >
+          <img class="hinhanh" alt=" hinhanh" src="img/anhphongnoibat/21.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="21" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/5.jpg" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/22.jpg" >
         </a>
       </li>
           <li >
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="22" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/6.jpg" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/23.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="23" >
-          <img class="hinhanh" alt=" hinhanh" src="img/anhphongnoibat/7.webp" >
+          <img class="hinhanh" alt=" hinhanh" src="img/anhphongnoibat/24.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="24" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/12.webp" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/25.jpg" >
         </a>
       </li>
           <li >
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="25" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/9.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/26.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="26" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/10.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/27.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="27" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/11.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/28.jpg" >
         </a>
       </li>
           <li >
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="28" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/8.webp" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/29.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="29" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/13.jpg" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/30.jpg" >
         </a>
       </li>
           <li class=" dau">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="30" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/14.jpg" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/31.jpg" >
         </a>
       </li>
           <li >
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="31" >
-          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/15.jpg" >
+          <img class="hinhanh" alt="hinhanh " src="img/anhphongnoibat/32.jpg" >
         </a>
       </li>
           <li class="cuoi">
         <a class="thanh_hinhanh" href="javascript:void(0);" data-id="32" >
-          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/16.jpg" >
+          <img class="hinhanh" alt="hinhanh" src="img/anhphongnoibat/33.jpg" >
         </a>
       </li>
         </ul>
