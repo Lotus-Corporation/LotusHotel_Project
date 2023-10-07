444fb9beb7972073a72c70dcfb3ca103d79f5e16
Thao301 <225481015@vaa.edu.vn>
2023-10-07 23:57:55 +0700

    -

--- Homepage/html/menu-logo.php
+++ Homepage/html/menu-logo.php
@@ -4,39 +4,6 @@
   $sql="select * from loaiphong";
   $result=$link->query($sql);
 ?>
-<<<<<<< HEAD
-      
-<div class="container">
-      <div style="width: 100%; position: fixed; z-index: 1; background-color:white;border: 5px solid rgb(193, 233, 244);">
-        <div class="logo1";>Our rooms
-        <div class="menu" >
-          
-        <?php
-while ($row=$result->fetch_assoc())
-{
- ?>   
-    <a> <?php echo $row["LOAIPHONG"];?></a>
-<?php
- }
-?>
-        </div>
-        </div>
-        <div class="logo1"><a style="text-decoration: none; color: black" href="view/public_dichvu.php" >Services</a></div>
-        <div class="logo1">Restaurant</div>
-        <div class="logo">
-          
-         <img src="img/LOGO.png" alt="" width=100% height=130px style="border: 5px solid rgb(193, 233, 244); "></div> 
-        <div class="logo1">Spa&Wellness</div>
-        <div class="logo1" ><a style="text-decoration: none; color: black" href="view/public_khuyenmai.php" >Special Offers</a></div>
-        <div class="logo1">Contacts</div>
-    
-      
-      </div>
-      <div align="center" style="width: 100%; height: 600px; background-color: white; position:relative; top:50px">
-      <?php include 'view/anhchinh.php';?>
-      </div>
-</div>
-=======
 <div class="thanh_logo1">
   <a class="icon" href="index_menu.php?pid=1">
     <img src="https://img.icons8.com/?size=256&id=2797&format=png" height="35px">
@@ -65,6 +32,5 @@ while ($row=$result->fetch_assoc())
   </a>
 </div>
       
->>>>>>> b2515492e39ef42ab153c23d6490f1d3b7aa4334
 
       
