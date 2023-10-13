708240772b16447fa3ccc5ce494ae7183ea62155
LIGHTHOUSE <haidang@vtv.vn>
2023-10-14 05:03:07 +0700

    phongnoibat-dv

--- Homepage/html/phongnoibat.php
+++ Homepage/html/phongnoibat.php
@@ -1,60 +1,44 @@
 <link rel="stylesheet" href="./css/phongnoibat.css"> 
+<?php 
+    $link=new mysqli("localhost","root","","khachsan");
+    $sql = "
+    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='STD' LIMIT 1)
+    UNION ALL
+    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='DLX' LIMIT 1)
+    UNION ALL
+    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='SUP' LIMIT 2)
+    UNION ALL
+    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='SUT' LIMIT 2)
+";
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "khachsan";
-
-// Tạo kết nối
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Kiểm tra kết nối
-if ($conn->connect_error) {
-  die("Kết nối thất bại: " . $conn->connect_error);
-}
-
-$sql = "SELECT DONGIAPHONG, LINK_ANH FROM PHONG";
-$result = $conn->query($sql);
-
-echo "<div id='phongnoibat'>
-        <div class='div_phongnoibat'>
-            <span class='hieuung left' style='display: block;'><span style='display: inline;'></span></span>
-            <span class='hieuung right' style='display: block;'><span style='display: inline;'></span></span>
-            
-          <ul class='ul'>";
-
-
-if ($result->num_rows > 0) {
-  // xuất dữ liệu của mỗi hàng
-  $i = 0;
-  while($row = $result->fetch_assoc()) {
-    $class = "";
-    if ($i % 3 == 0) {
-      $class = "dau";
-    } elseif ($i % 3 == 2) {
-      $class = "cuoi";
-    }
-    echo "<li class='" . $class . "'>
-            <a href='#' class='thanh_hinhanh'>
-              <img src='" . $row["LINK_ANH"] . "' alt='Phòng khách sạn' class='hinh_anh'>
-              <div class='overlayy'>
-                <p class='price'> Giá: " . $row["DONGIAPHONG"]. "VND</p>
-              </div>
-            </a>
-          </li>";
-    $i++;
-  }
-} else {
-  echo "0 results";
-}
-
-echo "</ul>
-    </div>
-    <a class='mo_toanbo' href='javascript:;'> Xem Tất Cả Các Phòng </a>
-  </div>";
-  
-$conn->close();
+    $result=$link->query($sql);
 ?>
 
+<div class="mau_layout" style="
+    height: 1100px;
+">
+    <div class="loigoi">
+        <h3 style=" font-weight: 100; " >một số phòng nổi bật của chúng tôi</h3>
+    </div>
+    <?php
+    while ($row=$result->fetch_assoc())
+    {
+    ?>
+    <div class="layout">
+        <a href="">
+            <div class="layout_con">
+                <img src="img/<?php echo $row["ANHPHONG"]; ?>">
+                <div class="price">Price:<b> <?php echo $row["DONGIAPHONG"] ?>đ</b></div>
+                <div  class="tenphong">
+                    <div><h2><?php echo $row["MA_LOAIPHONG"] ?></h2></div>
+                    <i class="fa fa-bed" aria-hidden="true">1 bed</i>
+                    <i class="fa fa-users" aria-hidden="true">Max <?php echo $row["SONGUOITOIDA"]?></i>
+                </div>
+            </div>
+        </a>
+    </div>
+    <?php
+    }
+    ?>
+</div> <!-- Đây là thẻ đóng cho .mau_layout -->
 
