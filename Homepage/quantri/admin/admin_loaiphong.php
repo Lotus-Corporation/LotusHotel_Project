8bb7a6dff8f1af7d5d6d9fead6f075f5197a485a
hinn <lehiennguyen107@gmail.com>
2023-10-14 12:22:02 +0700

    admin_khuyenmai

--- Homepage/quantri/admin/admin_loaiphong.php
+++ Homepage/quantri/admin/admin_loaiphong.php
@@ -4,11 +4,67 @@ $sql="select * from loaiphong";
 $result=$link->query($sql);
 ?>
 <style>
-    table{width:100%}
-    th,td{
-        border: 1px solid gray;
+    .layout_danhmuc{
+        width: 60%; 
+        position: absolute; 
+        background-color: rgb(253, 245, 233);
+        top:10%;
+        right:8%; 
+        height: 700px;
+        top:100px;
+        border-radius: 10px;
+    }
+    .danhmuc{
+        width: 100%;
+        height: 70px;
+        background-color: gray;
+        border-radius: 10px;
+        position: absolute;
+        top:0px;
+    }
+    button{
+        padding: 15px 35px;
+        position: absolute;
+        right: 2%;
+        top: 83px;
+        border: none;
+        background-color: rgb(210, 168, 84);
+        border-radius: 10px;
+        cursor: pointer;
+    }
+    button:hover{
+        background-color: rgb(161, 110, 7);
+    }
+    table{ 
+        width: 95%; 
+        margin: 9% 10% 0 3%; 
+        border-radius: 10px;
+        border-spacing: 0;
+        border-collapse: separate; 
+        top:10px;
+    }
+   
+    th{
+       background-color: gray;
+       padding: 10px;
+       border-radius: 7px;
+
+    }
+
+    td{
+        background-color:antiquewhite;
+        border-style:outset;
         padding: 10px;
-        text-align: center;
+        border-radius: 7px;
+
+    }
+   
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    a:hover{
+        color: brown;
     }
 </style>
 <table>
