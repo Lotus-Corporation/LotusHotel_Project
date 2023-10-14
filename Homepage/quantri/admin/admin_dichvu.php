87c45794a6393c680d265c852eeee8a87e1dfb5d
hinn <lehiennguyen107@gmail.com>
2023-10-14 13:39:42 +0700

    admin_nkdp

--- Homepage/quantri/admin/admin_dichvu.php
+++ Homepage/quantri/admin/admin_dichvu.php
@@ -4,20 +4,79 @@
     $result=$link->query($sql);
 ?>
 <style>
-    th, td{
-        border: 2px solid gray;
-        text-align: center;
-        padding: 10px
+     .layout_danhmuc{
+        width: 75%; 
+        position: absolute; 
+        background-color: rgb(253, 245, 233);
+        top:10%;
+        right:1%; 
+        height: 6330px;
+        top:65px;
     }
-    table{ width: 100%}
+    .danhmuc{
+        width: 100%;
+        height: 70px;
+        background-color: gray;
+        border-radius: 10px;
+        position: absolute;
+        top:0px;
+    }
+    button{
+        padding: 15px 35px;
+        position: absolute;
+        right: 2%;
+        top: 83px;
+        border: none;
+        background-color: rgb(210, 168, 84);
+        border-radius: 10px;
+        cursor: pointer;
+    }
+    button:hover{
+        background-color: rgb(161, 110, 7);
+    }
+    table{ 
+        width: 95%; 
+        margin: 9% 10% 0 3%; 
+        border-radius: 10px;
+        border-spacing: 0;
+        border-collapse: separate; 
+        top:10px;
+    }
+   
+    th{
+       background-color: gray;
+       padding: 10px;
+
+    }
+
+    td{
+        background-color:antiquewhite;
+        border-style:outset;
+        padding: 10px;
+        border-radius: 7px;
+
+    }
+   
+    a{
+        text-decoration: none;
+        color: black;
+    }
+    a:hover{
+        color: brown;
+    }
 </style>
+    <div class="layout_danhmuc"> 
+        <div class="danhmuc"><h2 style="margin: 2% 0 0 2%"> Danh Sách Dịch Vụ </h2>
+            <button>Thêm</button>
 <table>
     <tr>
-        <th>Mã dịch vụ</th>
+        <th style="border-radius: 10px 0 0 0">Mã dịch vụ</th>
         <th>Tên dịch vụ</th>
         <th>Đơn giá dịch vụ</th>
         <th>Đơn vị tính</th>
         <th>Hình Ảnh</th>
+        <th>Sửa</th>
+        <th style="border-radius: 0 10px 0 0">Xóa</th>
     </tr>
 
     <?php
@@ -30,6 +89,8 @@
                 <td><?php echo $row["DONGIADV"]; ?></td>
                 <td><?php echo $row["DONVITINH"]; ?></td>
                 <td><img src="../img/dichvu/<?php echo $row["HINHANHDV"]; ?>" style="width: 300px; height: 280px;"></td>
+                <td><a href=""><img src="../img/icon/edit.png"  width="10px" height="10px"> Sửa</a></td>
+                <td><a href=""><img src="../img/icon/delete.png" width="10px" height="10px"> Xóa</a></td>
             </tr>
     <?php
         }
