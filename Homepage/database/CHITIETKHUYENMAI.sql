2ac8b29380a4327c19ea9d561266e1a115309614
NHI <yennhisociuu2004@gmail.com>
2023-10-16 03:02:02 +0700

    sua database

--- Homepage/database/CHITIETKHUYENMAI.sql
+++ Homepage/database/CHITIETKHUYENMAI.sql
@@ -1,30 +1,79 @@
 create table CHITIETKHUYENMAI(
+    MA_CTKM varchar(10) primary key not null,
     MA_KM varchar(10),
     foreign key (MA_KM) references KHUYENMAI(MA_KM),
     GIATRI float not null,
-    MA_CTNKDP varchar(6),
-    foreign key (MA_CTNKDP) references CHITIETNHATKIDATPHONG(MA_CTNKDP),
+    MA_LOAIPHONG varchar(3),
+    foreign key (MA_LOAIPHONG) references LOAIPHONG(MA_LOAIPHONG),
     MA_DV varchar(10),
     foreign key (MA_DV) references DICHVU(MA_DV)
 ) ENGINE= InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-insert into CHITIETKHUYENMAI(MA_KM,GIATRI,MA_CTNKDP,MA_DV) values
-('km01',0.15,null,null),
-('km02',200000,null,null),
-('km03',0.1,'CTDP2',null),
-('km04',200000,null,'DV11'),
-('km05',0.05,'CTDP7',null),
-('km06',1000000,null,null),
-('km07',100000,null,'DV12'),
-('km08',100000,'CTDP10',null),
-('km08',200000,null,'DV09'),
-('km09',200000,'CTDP4',null),
-('km09',0.15,null,'DV07'),
-('km10',0.2,null,null),
-('km10',0.5,null,'DV04'),
-('km11',0.1,'CTDP17',null),
-('km11',0.1,null,'DV15'),
-('km11',0.1,null,'DV16'),
-('km12',0.1,null,'DV18'),
-('km12',0.1,null,'DV17'),
-('km12',0.1,null,'DV20')
+insert into CHITIETKHUYENMAI(MA_CTKM,MA_KM,GIATRI,MA_LOAIPHONG,MA_DV) values
+('CTKM01','km01',0.15,null,'DV01'),
+('CTKM02','km01',0.15,null,'DV02'),
+('CTKM03','km01',0.15,null,'DV03'),
+('CTKM04','km01',0.15,null,'DV04'),
+('CTKM05','km01',0.15,null,'DV05'),
+('CTKM06','km01',0.15,null,'DV06'),
+('CTKM07','km01',0.15,null,'DV07'),
+('CTKM08','km01',0.15,null,'DV08'),
+('CTKM09','km01',0.15,null,'DV09'),
+('CTKM10','km01',0.15,null,'DV10'),
+('CTKM11','km01',0.15,null,'DV11'),
+('CTKM12','km01',0.15,null,'DV12'),
+('CTKM13','km01',0.15,null,'DV13'),
+('CTKM14','km01',0.15,null,'DV14'),
+('CTKM15','km01',0.15,null,'DV15'),
+('CTKM16','km01',0.15,null,'DV16'),
+('CTKM17','km01',0.15,null,'DV17'),
+('CTKM18','km01',0.15,null,'DV18'),
+('CTKM19','km01',0.15,null,'DV19'),
+('CTKM20','km01',0.15,null,'DV20'),
+
+
+('CTKM21','km02',200000,null,null),
+
+
+('CTKM22','km03',0.1,'STD',null),
+('CTKM23','km03',0.1,'SUP',null),
+('CTKM24','km03',0.1,'DLX',null),
+('CTKM25','km03',0.1,'SUT',null),
+
+('CTKM26','km04',200000,null,'DV11'),
+
+('CTKM27','km05',0.05,'STD',null),
+('CTKM28','km05',0.05,'SUP',null),
+('CTKM29','km05',0.05,'DLX',null),
+('CTKM30','km05',0.05,'SUT',null),
+
+('CTKM31','km06',1000000,null,null),
+
+('CTKM32','km07',100000,null,'DV12'),
+
+('CTKM33','km08',100000,'STD',null),
+('CTKM34','km08',100000,'SUP',null),
+('CTKM35','km08',100000,'DLX',null),
+('CTKM36','km08',100000,'SUT',null),
+('CTKM37','km08',200000,null,'DV09'),
+
+
+('CTKM38','km09',200000,'STD',null),
+('CTKM39','km09',200000,'SUP',null),
+('CTKM40','km09',200000,'DLX',null),
+('CTKM41','km09',200000,'SUT',null),
+('CTKM42','km09',0.15,null,'DV07'),
+
+
+('CTKM43','km10',0.2,null,null),
+('CTKM44','km10',0.5,null,'DV04'),
+
+
+('CTKM45','km11',0.1,'SUT',null),
+('CTKM46','km11',0.1,null,'DV15'),
+('CTKM47','km11',0.1,null,'DV16'),
+
+
+('CTKM48','km12',0.1,null,'DV18'),
+('CTKM49','km12',0.1,null,'DV17'),
+('CTKM50','km12',0.1,null,'DV20')
--- Homepage/database/KHUYENMAI.sql
+++ Homepage/database/KHUYENMAI.sql
@@ -1,26 +1,22 @@
 create table KHUYENMAI(
     MA_KM varchar(10) primary key not null,
-    TENKM varchar(100),
-    MOTA varchar(200),
     NGAYBATDAU date,
     NGAYKETTHUC date,
-    DOITUONGAPDUNG varchar(100),
-    MA_LOAIPHONG varchar(3),
-    foreign key (MA_LOAIPHONG) references LOAIPHONG(MA_LOAIPHONG),
+    TENKM varchar(100),
+    MOTA varchar(200),
     HINHANHKM char(50)
 )ENGINE= InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-
 insert into KHUYENMAI values
-('km01','Hello Summer','Giảm 15% tất cả dịch vụ trong mùa hè.','2023-05-01','2023-07-31',null,null,'khuyenmai/km01.png'),
-('km02','Honeymoon - Tuần Trang Mật','Giảm 200k cho 3 đêm trở lên.','2023-10-1','2023-12-31','ngày trả-ngày đặt>3 ',null,'khuyenmai/km02.png'),
-('km03','Merry Christmas','Giảm 10%/phòng','2023-12-23','2023-12-26',null,null,'khuyenmai/km03.png'),
-('km04','Lễ 30/4-1/5','Bữa tối “Seafood Night” vào tối 30/4 - 1/5 tại nhà hàng chỉ với 350k/người.','2023-04-30','2023-05-01',null,null,'khuyenmai/km04.png'),
-('km05','Book online','Giảm 5% khi đặt phòng qua website.','2023-01-01','3000-01-01',null,null,'khuyenmai/km05.png'),
-('km06','Ưu đãi đặc biệt','Giảm 1.000.000đ khi hóa đơn trên 10.000.000đ','2023-01-01','3000-01-01','Tổng tiền > 10.000.000đ',null,'khuyenmai/km06.png'),
-('km07','Combo 3 bữa','Giảm 100k/người khi sử dụng combo ăn sáng/trưa/tối.','2023-01-01','3000-01-01',null,null,'khuyenmai/km07.png'),
-('km08','Happy weekend - Ưu đãi cuối tuần','Giảm 100k/phòng và miễn phí bữa sáng.','2023-09-22','2023-09-23',null,null,'khuyenmai/km08.png'),
-('km09','Lễ 2/9 Quốc Khánh','Giảm 200k/phòng và giảm 15%/dịch vụ Casino','2023-08-31','2023-09-02',null,null,'khuyenmai/km09.png'),
-('km10','Tri ân khách hàng thân thiết','Giảm 20% trên tổng hóa đơn và Giảm 50% / dịch vụ spa.','2023-01-01','3000-01-01','Tích điểm hóa đơn > 50đ',null,'khuyenmai/km10.png'),
-('km11','Ưu đãi phòng Suite','Giảm 10%/phòng, Giảm 10% dịch vụ chăm sóc trẻ và chăm sóc thú cưng','2023-01-01','3000-01-01',null,'SUT','khuyenmai/km11.png'),
-('km12','Sinh nhật hoành tráng','Giảm 10% Combo Spa-Fitness center, Bể bơi 4 mùa, Tổ chức tiệc trong nhà hàng theo yêu cầu/bàn' , '2023-01-01' , '2024-01-01',null,null,'khuyenmai/km12.png')
+('km01','2023/05/01','2023/07/31','Hello Summer','Giảm 15% tất cả dịch vụ trong mùa hè.','khuyenmai/km01.png'),
+('km02','2023/10/01','2023/12/31','Honeymoon - Tuần Trang Mật','Giảm 200k cho 3 đêm trở lên.','khuyenmai/km02.png'),
+('km03','2023/12/23','2023/12/26','Merry Christmas','Giảm 10%/phòng','khuyenmai/km03.png'),
+('km04','2023/04/30','2023/05/01','Lễ 30/4-1/5','Bữa tối “Seafood Night” vào tối 30/4 - 1/5 tại nhà hàng chỉ với 350k/người.','khuyenmai/km04.png'),
+('km05','2023/01/01','3000/01/01','Book online','Giảm 5% khi đặt phòng qua website.','khuyenmai/km05.png'),
+('km06','2023/01/01','3000/01/01','Ưu đãi đặc biệt','Giảm 1.000.000đ khi hóa đơn trên 10.000.000đ','khuyenmai/km06.png'),
+('km07','2023/01/01','3000/01/01','Combo 3 bữa','Giảm 100k/người khi sử dụng combo ăn sáng/trưa/tối.','khuyenmai/km07.png'),
+('km08','2023/05/05','2023/10/30','Happy weekend - Ưu đãi cuối tuần','Giảm 100k/phòng và miễn phí bữa sáng.','khuyenmai/km08.png'),
+('km09','2023/08/31','2023/09/02','Lễ 2/9 Quốc Khánh','Giảm 200k/phòng và giảm 15%/dịch vụ Casino','khuyenmai/km09.png'),
+('km10','2023/01/01','3000/01/01','Tri ân khách hàng thân thiết','Giảm 20% trên tổng hóa đơn và Giảm 50% / dịch vụ spa.','khuyenmai/km10.png'),
+('km11','2023/01/01','3000/01/01','Ưu đãi phòng Suite','Giảm 10%/phòng, Giảm 10% dịch vụ chăm sóc trẻ và chăm sóc thú cưng','khuyenmai/km11.png'),
+('km12','2023/01/01','2024/01/01','Sinh nhật hoành tráng','Giảm 10% Combo Spa-Fitness center, Bể bơi 4 mùa, Tổ chức tiệc trong nhà hàng theo yêu cầu/bàn','khuyenmai/km12.png')
