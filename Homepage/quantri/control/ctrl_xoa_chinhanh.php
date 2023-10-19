cdd89e27f61f263bd4d1df1e28bea82d0bba3fc0
Thao301 <146383323+Thao301@users.noreply.github.com>
2023-10-20 05:44:56 +0700

    Update ctrl_xoa_chinhanh.php

--- Homepage/quantri/control/ctrl_xoa_chinhanh.php
+++ Homepage/quantri/control/ctrl_xoa_chinhanh.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_REQUEST['ma_cn']) and $_REQUEST['ma_cn']!=""){
-    $ma = $_POST["ma_cn"];
+    $ma = $_GET["ma_cn"];
     $link = new mysqli("localhost", "root", "", "khachsan");
     $link->query("SET foreign_key_checks = 0");
     $sql = "DELETE FROM chinhanh WHERE ma_cn='$ma'";
