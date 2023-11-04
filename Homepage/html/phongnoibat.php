4201d041b9feb48a2a80c193f758d3d2595bd50d
THECOFFEEMINT <NEW.HORIZONS1001101@YAHOO.COM>
2023-11-04 22:06:23 +0700

    popup hien thi chi tiet phong

--- Homepage/html/phongnoibat.php
+++ Homepage/html/phongnoibat.php
@@ -1,72 +1,136 @@
-<link rel="stylesheet" href="./css/phongnoibat.css"> 
-<link rel="stylesheet" href="css/https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-<script src="./js/js.js"></script>
-<link rel="stylesheet" type="text/css" href="./css/slick.css"/>
-<script src="./js/js2.js"></script>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./css/phongnoibat.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <script src="./js/js.js"></script>
+    <link rel="stylesheet" type="text/css" href="./css/slick.css"/>
+    <script src="./js/js2.js"></script>
+</head>
+<body>
+    <?php 
+        $link = new mysqli("localhost", "root", "", "khachsan");
+        $sql = "
+            (SELECT * FROM PHONG WHERE MA_LOAIPHONG='STD' LIMIT 1)
+            UNION ALL
+            (SELECT * FROM PHONG WHERE MA_LOAIPHONG='DLX' LIMIT 1)
+            UNION ALL
+            (SELECT * FROM PHONG WHERE MA_LOAIPHONG='SUP' LIMIT 2)
+            UNION ALL
+            (SELECT * FROM PHONG WHERE MA_LOAIPHONG='SUT' LIMIT 2)
+        ";
+        $result = $link->query($sql);
+    ?>
 
-<?php 
-    $link=new mysqli("localhost","root","","khachsan");
-    $sql = "
-    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='STD' LIMIT 1)
-    UNION ALL
-    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='DLX' LIMIT 1)
-    UNION ALL
-    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='SUP' LIMIT 2)
-    UNION ALL
-    (SELECT * FROM PHONG WHERE MA_LOAIPHONG='SUT' LIMIT 2)
-";
+    <div class="mau_layout" style="height: auto; padding: 2em;">
+        <div class="loigoi">
+            <h3 style="font-weight: normal;">Một số phòng nổi bật của chúng tôi</h3>
+        </div>
 
-    $result=$link->query($sql);
-?>
-
-<div class="mau_layout" style="
-    height: auto;
-    padding: 2em;
-">
-    <div class="loigoi">
-        <h3  style="
-    font-weight: normal;
-">Một số phòng nổi bật của chúng tôi</h3>
+        <div class="carousel">
+            <?php
+                while ($row = $result->fetch_assoc()) {
+            ?>
+            <div class="layout">
+                <a>
+                    <div class="layout_con">
+                        <img src="img/phong/<?php echo $row["ANHPHONG"]; ?>">
+                        <div class="price">Price:<b> <?php echo $row["DONGIAPHONG"] ?>đ</b></div>
+                        <div class="cart"><i class="fa fa-shopping-cart"> Booking </i></div>
+                        <div class="tenphong">
+                            <div><h2><?php echo $row["TENPHONG"] ?></h2></div>
+                            <i class="fa fa-bed" aria-hidden="true"><?php echo $row["SOGIUONG"]?> bed</i>
+                            <i class="fa fa-users" aria-hidden="true">Max <?php echo $row["SONGUOITOIDA"]?></i>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <?php
+                }
+            ?>
+        </div>
     </div>
-    
 
-    <div class="carousel">
-        <?php
-        while ($row=$result->fetch_assoc())
-        {
-        ?>
-        <div class="layout">
-            <a href="">
-                <div class="layout_con">
-                    <img src="img/phong/<?php echo $row["ANHPHONG"]; ?>">
-                    <div class="price">Price:<b> <?php echo $row["DONGIAPHONG"] ?>đ</b></div>
-                    <div class="cart"><i class="fa fa-shopping-cart"> Add to cart</i></div>
-                    <div  class="tenphong">
-                        <div><h2><?php echo $row["TENPHONG"] ?></h2></div>
-                        <i class="fa fa-bed" aria-hidden="true"><?php echo $row["SOGIUONG"]?> bed</i>
-                        <i class="fa fa-users" aria-hidden="true">Max <?php echo $row["SONGUOITOIDA"]?></i>
+    <div id="myModal" class="modal">
+        <div class="modal-content">
+            <div class="header">
+                <h2 id="modalTitle"></h2>
+                <span class="close">×</span>
+            </div>
+            <hr>
+            <div class="body">
+                <div class="hinhphong">
+                    <img id="img01">
+                </div>
+                <div class="motaphong">
+                    <div class="description">
+                        Được thiết kế sang trọng và hoàn hảo với các tiện nghi hiện đại, đáp ứng tất cả kỳ nghỉ thư thái của bạn trong căn phòng yên tĩnh với cửa sổ lớn nhìn ra quang cảnh thành phố.
+                    </div>
+                    <P>Tiện nghi của phòng</P>
+                    <div class="amenities">
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-wardrobe-furniture-household-prettycons-solid-prettycons_1678159280.png"> Tủ quần áo</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/bed--v1_1678159161.png"> Ga trải giường, gối</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/no-smoking_1678159728.png"> Phòng không hút thuốc</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/shower-on--v1_1678159438.png"> Vòi sen</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-bar-counter-bar-and-restaurant-icongeek26-glyph-icongeek26_1678159932.png"> Quầy bar mini</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/shower_1678160210.png"> Phòng tắm - Vòi sen</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-laundry-cleaning-kiranshastry-solid-kiranshastry-1_1678159358.png"> Dịch vụ giặt ủi</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/hair-dryer_1678159642.png"> Máy sấy tóc</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/bank-safe_1678160287.png"> Két sắt an toàn</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-wifi-connection-overclocking-smashingstocks-glyph-smashing-stocks_1678160128.png"> Wifi</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/soap_1678158691.png"> Đồ phòng tắm</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/towel_1678158562.png"> Khăn tắm</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-telephone-communications-prettycons-solid-prettycons_1678158776.png"> Điện thoại</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/desk-lamp_1678158899.png"> Đèn bàn</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-desk-back-to-school-vitaliy-gorbachev-fill-vitaly-gorbachev_1678158472.png"> Bàn làm việc</div>
                     </div>
                 </div>
-            </a>
+            </div>
         </div>
-        <?php
-        }
-        ?>
     </div>
-</div>
 
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.carousel').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 1500,
+                prevArrow: '<button type="button" class="slick-prev" style="background: transparent; border: none;"> <img src="./img/anhphongnoibat/mt.png" alt="some_text" style="opacity:0.4;filter:alpha(opacity=40); padding: 0 0 0 29px;;"/></button>',
+                nextArrow: '<button type="button" class="slick-next" style="background: transparent; border: none; padding: 4px"> <img src="./img/anhphongnoibat/mt2.png" alt="some_text" style="opacity:0.4;filter:alpha(opacity=40)"/></button>'
+            });
 
+            var modal = document.getElementById("myModal");
+            var layoutCons = document.getElementsByClassName("layout_con");
+            var modalImg = document.getElementById("img01");
+            var modalTitle = document.getElementById("modalTitle");
 
-<script type="text/javascript">
-$(document).ready(function(){
-  $('.carousel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed:1500,
-    prevArrow: '<button type="button" class="slick-prev" style="background: transparent; border: none;  "> <img src="./img/anhphongnoibat/mt.png" alt="some_text" style="opacity:0.4;filter:alpha(opacity=40); padding: 0 0 0 29px;;"/></button>',
-    nextArrow: '<button type="button" class="slick-next" style="background: transparent; border: none; padding: 4px"> <img src="./img/anhphongnoibat/mt2.png" alt="some_text" style="opacity:0.4;filter:alpha(opacity=40)"/></button>'
-  });
-});
-</script>
+            for(let i = 0; i < layoutCons.length; i++) {
+                layoutCons[i].onclick = function() {
+                    modal.style.display = "block";
+                    modalImg.src = this.getElementsByTagName('img')[0].src;
+                    modalTitle.innerHTML = this.getElementsByClassName('tenphong')[0].getElementsByTagName('h2')[0].innerHTML;
+                }
+            }
 
+            var span = document.getElementsByClassName("close")[0];
+
+            span.onclick = function() { 
+                modal.style.display = "none";
+            }
+
+            window.onclick = function(event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+
+            window.addEventListener("keydown", function(event) {
+                if (event.key === "Escape") {
+                    modal.style.display = "none";
+                }
+            });
+        });
+    </script>
+</body>
+</html>
--- Homepage/view/public_loaiphong.php
+++ Homepage/view/public_loaiphong.php
@@ -1,3 +1,4 @@
+
 <?php 
     $link=new mysqli("localhost","root","","khachsan");
     $mlp=$_GET["malp"];
@@ -6,6 +7,7 @@
 ?>
 
 <style>
+    /* Your existing CSS */
     body{
         background-color:aliceblue;
         margin: 0;
@@ -85,6 +87,128 @@
         transform: scale(1.07);
         opacity: 0.93;
     }
+
+
+    .modal {
+    display: none;
+    position: fixed;
+    z-index: 6;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0,0,0,0.4);
+
+  }
+  
+  .modal-content {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 2px 10px rgba(0,0,0,0.2);
+    margin: 10%;
+    top: -9%;
+  }
+  .modal-content .body .motaphong .amenities .amenity {
+    width: 48%;
+    display: flex;
+    align-items: center;
+    margin: 3% 1% 2% 0;
+    
+
+}
+    
+  .modal-content .header {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: #000;
+  }
+  
+  .modal-content .header h2 {
+    margin: 16px;
+    padding-left: 41%;
+  }
+  
+  .modal-content .header .close {
+    color: #000;
+    font-size: 26px;
+    font-weight: bold;
+    transition: 0.3s;
+    cursor: pointer;
+    text-decoration: none;
+    border-radius: 50%;
+    padding: 2px;
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  
+  .modal-content .body {
+    display: flex;
+    width: 100%;
+    height: 90%;
+  }
+  
+  .modal-content .body .hinhphong {
+    width: 66%;
+  }
+  
+  .modal-content .body .hinhphong img {
+    width: 100%;
+    max-height: 100%;
+  }
+  
+  .modal-content .body .motaphong {
+    width: 30%;
+    color: #000;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: space-between; */
+    margin: 1% 2% 2% 2%;
+
+  }
+  
+  .modal-content .body .motaphong .description {
+    height: 28%;
+  }
+  
+  .modal-content .body .motaphong .amenities {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+   
+  }
+  
+  .modal-content .body .motaphong .amenities .amenity {
+    width: 48%;
+    display: flex;
+    align-items: center;
+    margin: 3% 1% 2% 0;
+}
+  
+  .modal-content .body .motaphong .amenities .amenity img {
+    width: 20px;
+    height: 20px;
+    margin-right: 5px;
+  }
+  
+  .modal-content hr {
+    border: 0;
+    border-top: 1px solid #ccc;
+    margin: 10px 0;
+    width: 95%;
+  }
+  
 </style>
 
 <div class="layout_anh">
@@ -98,9 +222,9 @@
     {
 ?>
 <div class="layout">
-    <a href="">
-        <div class="layout_con">
-            <img src="img/phong/<?php echo $row["ANHPHONG"]; ?>">
+    <a href="#popup<?php echo $row["MA_PHONG"]; ?>">
+    <div class="layout_con">
+    <img src="img/phong/<?php echo $row["ANHPHONG"]; ?>" onclick="openPopup('popup<?php echo $row["MA_PHONG"]; ?>', '<?php echo $row["TENPHONG"]; ?>', 'img/phong/<?php echo $row["ANHPHONG"]; ?>')">
             <div class="price">Price:<b> <?php echo $row["DONGIAPHONG"] ?>đ</b></div>
             <form method="POST" action="view/public_giohang.php?action=add">
                 <input type=number value="1" name="soluongphong[<?php echo $row["MA_PHONG"]?>]">
@@ -115,6 +239,95 @@
         </div>
     </a>
 </div>
+
+
+<div id="popup<?php echo $row["MA_PHONG"]; ?>" class="modal">
+    <div class="modal-content">
+        <div class="header">
+            <h2 id="modalTitle<?php echo $row["MA_PHONG"]; ?>"></h2>
+            <span class="close">×</span>
+        </div>
+        <hr>
+        <div class="body">
+            <div class="hinhphong">
+                <img id="img01<?php echo $row["MA_PHONG"]; ?>">
+            </div>
+            <div class="motaphong">
+                <div class="description">
+                    Được thiết kế sang trọng và hoàn hảo với các tiện nghi hiện đại, đáp ứng tất cả kỳ nghỉ thư thái của bạn trong căn phòng yên tĩnh với cửa sổ lớn nhìn ra quang cảnh thành phố.
+                </div>
+                <P>Tiện nghi của phòng</P>
+                <div class="amenities">
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-wardrobe-furniture-household-prettycons-solid-prettycons_1678159280.png"> Tủ quần áo</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/bed--v1_1678159161.png"> Ga trải giường, gối</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/no-smoking_1678159728.png"> Phòng không hút thuốc</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/shower-on--v1_1678159438.png"> Vòi sen</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-bar-counter-bar-and-restaurant-icongeek26-glyph-icongeek26_1678159932.png"> Quầy bar mini</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/shower_1678160210.png"> Phòng tắm - Vòi sen</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-laundry-cleaning-kiranshastry-solid-kiranshastry-1_1678159358.png"> Dịch vụ giặt ủi</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/hair-dryer_1678159642.png"> Máy sấy tóc</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/bank-safe_1678160287.png"> Két sắt an toàn</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-wifi-connection-overclocking-smashingstocks-glyph-smashing-stocks_1678160128.png"> Wifi</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/soap_1678158691.png"> Đồ phòng tắm</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/towel_1678158562.png"> Khăn tắm</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-telephone-communications-prettycons-solid-prettycons_1678158776.png"> Điện thoại</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/desk-lamp_1678158899.png"> Đèn bàn</div>
+                        <div class="amenity"><img src="https://booking.muongthanh.com/images/rooms/service/2023/03/original/external-desk-back-to-school-vitaliy-gorbachev-fill-vitaly-gorbachev_1678158472.png"> Bàn làm việc</div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+
+var popups = document.getElementsByClassName("modal");
+
+
+var spans = document.getElementsByClassName("close");
+
+
+function openPopup(popupId, title, imgSrc) {
+    var popup = document.getElementById(popupId);
+    var modalTitle = document.getElementById("modalTitle" + popupId.replace("popup", ""));
+    var modalImg = document.getElementById("img01" + popupId.replace("popup", ""));
+    modalTitle.innerHTML = title;
+    modalImg.src = imgSrc;
+    popup.style.display = "block";
+}
+
+for (let i = 0; i < spans.length; i++) {
+    spans[i].onclick = function() {
+        popups[i].style.display = "none";
+    }
+}
+
+
+window.onclick = function(event) {
+    for (let i = 0; i < popups.length; i++) {
+        if (event.target == popups[i]) {
+            popups[i].style.display = "none";
+        }
+    }
+}
+
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {
+        for (let i = 0; i < popups.length; i++) {
+            popups[i].style.display = "none";
+        }
+    }
+};
+</script>
+
 <?php
     }
 ?>
